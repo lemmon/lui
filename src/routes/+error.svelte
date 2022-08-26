2ef2fc59656c0e8e302edf2c1b69e3f25912f30c
Jakub Pelák <jpelak@gmail.com>
2022-08-26 10:50:11 +0200

    svelte kit update

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+import { page } from '$app/stores'
+</script>
+
+<svelte:head>
+  <title>SŪIL Error {$page.status}</title>
+</svelte:head>
+
+<div class="span1 p2">
+  <h1 class="h1 lh2 lsh">Error {$page.status}</h1>
+
+  <p class="mt2 lh4">{$page.error.message}</p>
+</div>
