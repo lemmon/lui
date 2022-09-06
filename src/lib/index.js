fc766cf8b32a03ac21133b31f82d6fcdc6260441
Jakub Pelák <jpelak@gmail.com>
2022-09-06 10:24:30 +0200

    fix export

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
 export { default as Button } from './Button.svelte'
 export { default as Code } from './Code.svelte'
 export { default as CopyButton } from './CopyButton.svelte'
-export { default as Dump } from './CopyButton.svelte'
+export { default as Dump } from './Dump.svelte'
