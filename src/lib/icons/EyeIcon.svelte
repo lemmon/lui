833e5f671031aaa29aac7aa8bc01e65836511561
Jakub Pelák <jpelak@gmail.com>
2022-11-23 13:40:23 +0100

    PasswordInput

---
+++ src/lib/icons/EyeIcon.svelte
@@ -0,0 +1,14 @@
+<svg
+  class={$$props.class}
+  style={$$props.style}
+  style:display="block"
+  width="20"
+  height="20"
+  viewBox="0 0 20 20"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="1"
+>
+  <circle cx="10" cy="10" r="3" />
+  <path d="M 1 10 C 6 2 14 2 19 10 C 14 18 6 18 1 10Z" />
+</svg>
---
+++ src/routes/dribbble/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+import TextInput from '$lib/TextInput.svelte'
+import PasswordInput from '$lib/PasswordInput.svelte'
+import NumberInput from '$lib/NumberInput.svelte'
+import TextArea from '$lib/TextArea.svelte'
+</script>
+
+<div class="span1 col">
+  <div class="view bg-blue bg-sat10 bg-lum80 col justify-center px4 ma">
+    <div class="window bg-white">
+      <div class="col gap1 p2">
+        <TextInput label="Text" value="Hello World!" />
+        <PasswordInput label="Password" value="12345678" error="Err, this password is no good." />
+        <NumberInput label="Number" value={42} />
+        <TextArea
+          label="Text"
+          info="Place for some helpful information."
+          value="Mauris molestie purus egestas elit fermentum porta. Sed et placerat ligula, vel molestie nulla. Phasellus nisl nulla, pulvinar quis finibus eget, sollicitudin eu neque."
+        />
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+.view {
+  width: 800px;
+  height: 600px;
+}
+</style>
